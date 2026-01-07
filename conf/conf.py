70bd22ec6769ce25b590f422db651a2546adef0f
killias35 <killias356@gmail.com>
2026-01-07 16:56:28 +0100

    V 1.2

--- conf/conf.py
+++ conf/conf.py
@@ -12,14 +12,22 @@ ANNUAIRE_EXPERT_COMPTABLE_URL = "https://annuaire.experts-comptables.org/tous-le
 SOCIETE_COM_URL = "https://www.societe.com/"
 
 def set_parameters():
-    global VILLE, FILE_PATH
+    global VILLES, FILES_PATH
     if os.path.exists("parameters.json"):
         with open("parameters.json", "r") as f:
             parameters = json.load(f)
-            VILLE = parameters.get("villes", VILLES)
-            FILE_PATH = parameters.get("chemins_fichier", FILES_PATH)
+            VILLES = parameters.get("villes", VILLES)
+            FILES_PATH = parameters.get("chemins_fichier", FILES_PATH)
     else:
         print("parameters.json n'existe pas, création avec valeurs par defaut...")
         with open("parameters.json", "w") as f:
             json.dump({"villes": VILLES, "chemins_fichier": FILES_PATH}, f, indent=4)
+
+def is_files_valid():
+    for i in range(len(FILES_PATH)):
+        if not os.path.exists(FILES_PATH[i]):
+            return False, f"Le fichier {FILES_PATH[i]} n'existe pas"
+    return True, "Tout les fichiers existent"
+
+
 set_parameters()
--- main.py
+++ main.py
@@ -3,32 +3,35 @@ from data.utils.session import Session
 from data.get_file_data import get_file_datas
 from data.get_pappers_datas import get_pappers_datas
 from data.utils.export_to_csv import export_to_csv
-from conf.conf import VILLES, FILES_PATH
+from conf.conf import VILLES, FILES_PATH, is_files_valid
 import traceback
 
 def main():
     print("Debut du script")
-    print("Chargement du naviguateur...")
-    session = Session()
-        
-    try:
-        for i in range(len(VILLES)):
-            VILLE = VILLES[i]
-            FILE_PATH = FILES_PATH[i]
+    print("Verification des fichiers...")
+    ret, msg = is_files_valid()
+    print(msg)
+    if ret:
+        print("Chargement du naviguateur...")
+        session = Session()
+        try:
+            for i in range(len(VILLES)):
+                VILLE = VILLES[i]
+                FILE_PATH = FILES_PATH[i]
+                companies = get_file_datas(FILE_PATH)
+                scrapped_companies = get_pappers_datas(session, companies, VILLE)
+                session.close()
+                export_to_csv(scrapped_companies)
+                print(f"Entreprise dans le fichier: {len(companies)}\nEntreprise trouvées: {len(scrapped_companies)}")
 
-            companies = get_file_datas(FILE_PATH)
-            scrapped_companies = get_pappers_datas(session, companies, VILLE)
+        except Exception:
             session.close()
-            export_to_csv(scrapped_companies)
-            print(f"Entreprise dans le fichier: {len(companies)}\nEntreprise trouvées: {len(scrapped_companies)}")
-
-    except Exception:
-        session.close()
-        print("une erreur est apparue, un fichier 'debug.txt' a été crée contenant l'erreur, merci de me l'envoyer stp")
-        with open("debug.txt", "w") as f:
-            f.write(traceback.format_exc())
-        time.sleep(10000)
-    
+            print("une erreur est apparue, un fichier 'debug.txt' a été crée contenant l'erreur, merci de me l'envoyer stp")
+            with open("debug.txt", "w") as f:
+                f.write(traceback.format_exc())
+            time.sleep(10000)
+    else:
+        print("fin du script...")
 
 if __name__ == "__main__": 
     main()
